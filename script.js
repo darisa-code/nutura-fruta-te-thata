ce869d3b67b9ec80e1d0b4510835bd383a34d216
darisa <dresstoimresscompany@gmail.com>
2026-04-29 21:43:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,45 @@
 const products = [
-    { name:"Molla e Thata", price:3.5, image:"images/molla.jpg", description:"Molla organike e thatë" },
-    { name:"Bajame Premium", price:5.0, image:"images/bajame.jpg", description:"Bajame të shëndetshme" },
-    { name:"Fik i Thate", price:4.2, image:"images/fik.jpg", description:"Fik të thatë natyral" },
-    { name:"Arrë Kokosi", price:2.8, image:"images/kokosi.jpg", description:"Arrë kokosi e freskët" },
-    { name:"Bajame e Kripur", price:5.5, image:"images/bajame2.jpg", description:"Bajame me kripë natyrale" },
-    { name:"Molla e Kuqe", price:3.8, image:"images/molla2.jpg", description:"Molla e kuqe dhe e ëmbël" },
-    { name:"Fik i Kuq", price:4.5, image:"images/fik2.jpg", description:"Fik i kuq natyral" },
-    { name:"Arrë Panxhari", price:6.0, image:"images/arre.jpg", description:"Arrë e shijshme" },
-    { name:"Kajsia e Thate", price:3.2, image:"images/kajsia.jpg", description:"Kajsia organike e thatë" },
-    { name:"Rrush i Thate", price:2.9, image:"images/rrush.jpg", description:"Rrush i thatë premium" }
+"Arra","Arra Braziliane","Stika","Stika e kuqe","Bajame",
+"Qiqra te pjekura","Stika te qeruara","Lajthi e pjekur",
+"Rrush i zi","Kumbulla","Hurma arabe","Kajsi",
+"Boronica e tropojes","Fara kungulli te qeruara","Huamitos",
+"Miser","Lajthi vendi","Kikirik i pjekur me kripe",
+"Papaya","Fik i thate","Bajame vendi","Kivi i thate",
+"Arra pecan","Hurma parajsa","Hurma xheneti",
+"Arre makadame","Ullij berati","Fasule","Vaj ulliri",
+"Fasule te zeze","Pllaqi","Mjalte konispoli"
 ];
 
 const grid = document.getElementById("productGrid");
 
-products.forEach((product,index)=>{
+products.forEach((name,i)=>{
     grid.innerHTML += `
-        <div class="product" onclick="openModal(${index})">
-            <img src="${product.image}" alt="${product.name}">
-            <h3>${product.name}</h3>
-            <p>€${product.price}</p>
+        <div class="product" onclick="openModal(${i})">
+            <img src="images/${i+1}.jpg">
+            <h4>${name}</h4>
         </div>
     `;
 });
 
-const modal = document.getElementById("productModal");
+const modal = document.getElementById("modal");
 const modalName = document.getElementById("modalName");
 const modalDesc = document.getElementById("modalDesc");
 const modalPrice = document.getElementById("modalPrice");
-const modalImage = document.getElementById("modalImage");
+const modalImg = document.getElementById("modalImg");
 
-let selectedProductIndex;
-
-function openModal(index){
-    selectedProductIndex = index;
-    const product = products[index];
-    modal.style.display = "block";
-    modalName.innerText = product.name;
-    modalDesc.innerText = product.description;
-    modalPrice.innerText = "€" + product.price;
-    modalImage.src = product.image;
+function openModal(i){
+    modal.style.display="block";
+    modalName.innerText = products[i];
+    modalDesc.innerText = "Përshkrimi i produktit (e shkruan vetë)";
+    modalPrice.innerText = "Çmimi: €5.00";
+    modalImg.src = "images/" + (i+1) + ".jpg";
 }
 
 function closeModal(){
-    modal.style.display = "none";
+    modal.style.display="none";
 }
 
-function addToCartModal(){
-    const product = products[selectedProductIndex];
-    alert(`Ke porositur: ${product.name} për €${product.price}`);
-    closeModal();
+function orderNow(){
+    alert("Porosia u krye (mund ta lidhim me WhatsApp ose Stripe)");
 }
-
-
 
